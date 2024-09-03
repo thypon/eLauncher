09de87696a3c22d2ef06ad5d77273003f015a631
Andrea Brancaleoni <abc@pompel.me>
2024-09-03 02:19:42 +0200

    SettingsActivity: restart on swipe up too

--- app/src/main/java/me/pompel/elauncher/SettingsActivity.java
+++ app/src/main/java/me/pompel/elauncher/SettingsActivity.java
@@ -28,12 +28,30 @@ public class SettingsActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
-        gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
-            @Override
-            public void onLongPress(MotionEvent e) {
-                restartApplication();
+        gestureDetector = new GestureDetector(this, new GestureListener());
+    }
+
+    private class GestureListener extends GestureDetector.SimpleOnGestureListener {
+        private static final int SWIPE_THRESHOLD = 100;
+        private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            float diffY = e2.getY() - e1.getY();
+            float diffX = e2.getX() - e1.getX();
+            if (Math.abs(diffY) > Math.abs(diffX)) {
+                if (diffY < 0 && Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                    restartApplication();
+                    return true;
+                }
             }
-        });
+            return false;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+            restartApplication();
+        }
     }
 
     @Override
